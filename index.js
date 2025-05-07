2101d475a87fcf3eeb923292d1e35e4e67ac876a
ofek29 <ofek.av95@gmail.com>
2025-05-07 16:02:55 +0300

    First plugin implementation

---
+++ index.js
@@ -0,0 +1,55 @@
+'use strict'
+
+const fp = require('fastify-plugin')
+const { GlideClient } = require('@valkey/valkey-glide')
+
+async function fastifyValkey (fastify, options) {
+  const { namespace, closeClient = false, ...valkeyOptions } = options
+
+  let client = options.client || null
+
+  if (namespace) {
+    if (!fastify.valkey) {
+      fastify.decorate('valkey', Object.create(null))
+    }
+    if (fastify.valkey[namespace]) {
+      throw new Error(`Valkey '${namespace}' instance namespace has already been registered`)
+    }
+
+    const closeNamedInstance = (fastify) => { fastify.valkey[namespace].close() }
+
+    client = await setupClient(fastify, client, closeClient, valkeyOptions, closeNamedInstance)
+
+    fastify.valkey[namespace] = client
+  } else {
+    if (fastify.valkey) {
+      throw new Error('@fastify/valkey has already been registered')
+    }
+
+    const close = async (fastify) => { fastify.valkey.close() }
+
+    client = await setupClient(fastify, client, closeClient, valkeyOptions, close)
+
+    fastify.decorate('valkey', client)
+  }
+}
+
+async function setupClient (fastify, client, closeClient, valkeyOptions, closeInstance) {
+  if (client) {
+    if (closeClient === true) {
+      fastify.addHook('onClose', closeInstance)
+    }
+  } else {
+    client = await GlideClient.createClient(valkeyOptions)
+
+    fastify.addHook('onClose', closeInstance)
+  }
+  return client
+}
+
+module.exports = fp(fastifyValkey, {
+  fastify: '5.x',
+  name: '@fastify/valkey'
+})
+module.exports.default = fastifyValkey
+module.exports.fastifyValkey = fastifyValkey
