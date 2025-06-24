dc868769bc1e400df966ca436fb76e2decfedf26
ofek29 <ofek.av95@gmail.com>
2025-06-24 10:05:00 +0300

    fix: add plugin timout to bad address test

--- test/index.test.js
+++ test/index.test.js
@@ -315,7 +315,7 @@ test('Should be able to register multiple namespaced @fastify/valkey instances',
 test('Should throw when @fastify/valkey is initialized with an option that makes valkey throw', async (t) => {
   t.plan(1)
 
-  const fastify = Fastify()
+  const fastify = Fastify({ pluginTimeout: 20000 })
   t.after(() => fastify.close())
 
   fastify.register(fastifyValkey, { addresses: [] })
@@ -326,7 +326,7 @@ test('Should throw when @fastify/valkey is initialized with an option that makes
 test('Should throw when @fastify/valkey is initialized with a namespace and an option that makes valkey throw', async (t) => {
   t.plan(1)
 
-  const fastify = Fastify()
+  const fastify = Fastify({ pluginTimeout: 20000 })
   t.after(() => fastify.close())
 
   fastify.register(fastifyValkey, {
