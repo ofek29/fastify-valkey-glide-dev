41eeda92e7562c48fc6332aec2b020a6fed0f0f2
ofek29 <ofek.av95@gmail.com>
2025-09-20 17:49:57 +0300

    fix: add plugin timeout

--- test/index.test.js
+++ test/index.test.js
@@ -315,10 +315,10 @@ test('Should be able to register multiple namespaced @fastify/valkey instances',
 test('Should throw when @fastify/valkey is initialized with an option that makes valkey throw', async (t) => {
   t.plan(1)
 
-  const fastify = Fastify()
+  const fastify = Fastify({ pluginTimeout: 20000 })
   t.after(() => fastify.close())
 
-  fastify.register(fastifyValkey, { addresses: [], requestTimeout: 250 })
+  fastify.register(fastifyValkey, { addresses: [] })
 
   await t.assert.rejects(fastify.ready())
 })
@@ -326,7 +326,7 @@ test('Should throw when @fastify/valkey is initialized with an option that makes
 test('Should throw when @fastify/valkey is initialized with a namespace and an option that makes valkey throw', async (t) => {
   t.plan(1)
 
-  const fastify = Fastify()
+  const fastify = Fastify({ pluginTimeout: 20000 })
   t.after(() => fastify.close())
 
   fastify.register(fastifyValkey, {
